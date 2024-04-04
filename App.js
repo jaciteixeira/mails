37bf13738f301e2c124f6f1d2084365a9de9f36a
jaciteixeira <jaci9306@gmail.com>
2024-04-04 14:00:34 -0300

    finalizado emails

--- App.js
+++ App.js
@@ -1,66 +1,129 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Image, FlatList } from 'react-native';
-import React, { useState, useEffect } from 'react';
-import axios from 'react-native-axios';
-import { Icon } from 'react-native-paper';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Image,
+  FlatList,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import axios from "react-native-axios";
+import { SimpleLineIcons } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import { Icon } from "react-native-paper";
 
 export default function App() {
+  const [dados, setDados] = useState([]);
 
-  const [email, setEmail] = useState()
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://c2367643c1af4b37b25fea7b0b974818.api.mockbin.io/");
+        setDados(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar usuario:", error);
+      }
+    };
 
-  const dados = async () => {
-    try {
-      axios.get('https://run.mocky.io/v3/7ce6329c-3d7f-461e-8197-5228d0713b5a')
-      const data = response.data;
-      console.log(data);
-      console.log(data.avatar);
-    } catch (error) {
-      console.error('Erro ao buscar usuario:', error);
-    }
-  }; 
+    fetchData();
+  }, []);
 
-  const emails = [
+  const data = [
     {
-      "id": 1,
-      "sender": "Ralph Edwards",
-      "subject": "The results to our user testing",
-      "preview": "What's the progress on that task?",
-      "avatar": "http://placekitten.com/200/200",
-      "time": "02:02 am",
-      "attachments": true
+      attachments: true,
+      avatar: "https://i.pravatar.cc/700",
+      id: 1,
+      preview: "What's the progress on that task?",
+      sender: "Ralph Edwards",
+      subject: "The results to our user testing",
+      time: "02:02 am",
     },
     {
-      "id": 2,
-      "sender": "Eleanor Pena",
-      "subject": "Some notes",
-      "preview": "Yeah! You're right.",
-      "avatar": "http://placekitten.com/200/201",
-      "time": "05:51 am",
-      "attachments": false
+      attachments: false,
+      avatar: "https://i.pravatar.cc/100",
+      id: 2,
+      preview: "Yeah! You're right.",
+      sender: "Eleanor Pena",
+      subject: "Some notes",
+      time: "05:51 am",
     },
-  ]
-  
+    {
+      attachments: false,
+      avatar: "https://i.pravatar.cc/589",
+      id: 3,
+      preview: "What's the progress on that task?",
+      sender: "Leslie Alexander",
+      subject: "Your account with us",
+      time: "11:27 pm",
+    },
+    {
+      attachments: true,
+      avatar: "https://i.pravatar.cc/270",
+      id: 4,
+      preview: "So, what's your plan this weekend?",
+      sender: "Esther Howard",
+      subject: "Welcome to our mailing list",
+      time: "11:27 pm",
+    },
+    {
+      attachments: true,
+      avatar: "https://i.pravatar.cc/290",
+      id: 5,
+      preview: "I hope it goes well.",
+      sender: "Jenny Wilson",
+      subject: "The results to our user testing",
+      time: "04:02 am",
+    },
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>Mail</Text>
-      <View style={styles.input}>
-        <TextInput 
-          placeholder="Pesquisar ..." 
-          style={styles.textInput}
+      <View style={styles.appTitle}>
+        {/* <Image source={require('../../assets/paper-plane.png')} style={styles.appLogo} /> */}
+        <Text style={styles.appTitleText}>e-mail</Text>
+      </View>
+
+      <View style={styles.containerInput}>
+        <SimpleLineIcons
+          name="menu"
+          size={24}
+          color="black"
+          style={styles.menuIcon}
         />
+        <TextInput
+          placeholder="Search in the e-mail"
+          style={styles.inputTitle}
+        />
+        <Image
+          source={{ uri: "https://i.pravatar.cc/770" }}
+          style={styles.loggedUserAvatar}
+        />
+      </View>
+      {/* <Text style={styles.titleRecieved}>RECIEVED</Text> */}
+      {/* <Text style={styles.titleRecieved}>{dados.avatar}</Text> */}
+
       <FlatList
         data={dados}
         keyExtractor={(item) => item.id.toString()}
-        renderItem={({item}) => (
-          <View style={styles.emailItems}>
-            <Image source={{url: item.avatar}}></Image>
-            <View>
-              
+        renderItem={({ item }) => (
+          <View style={styles.containerEmail}>
+            <Image source={{ uri: item.avatar }} style={styles.userAvatar} />
+            <View style={styles.emailContent}>
+              <Text style={styles.emailSender}>{item.sender}</Text>
+              <Text style={styles.emailSubject}>{item.subject}</Text>
+              <Text style={styles.emailBody}>{item.preview}</Text>
+              {item.attachments ? (
+                <View style={styles.emailAttachment}>
+                  <Entypo name="attachment" size={24} color="black" />
+                  <Text style={styles.emailAttachmentText}>Anexo</Text>
+                </View>
+              ) : null}
             </View>
+            <Text style={styles.emailTime}>{item.time}</Text>
           </View>
         )}
       />
-      </View>
     </View>
   );
 }
@@ -68,21 +131,87 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 52,
+    backgroundColor: "#fff",
   },
-  input: {
-    width: '100%',
+  appLogo: {
+    width: 40,
+    height: 40,
+    alignSelf: "center",
+    marginVertical: 20,
+  },
+  appTitle: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: -20,
+  },
+  appTitleText: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginLeft: 10,
+    marginBottom: 8,
+  },
+  containerInput: {
+    flexDirection: "row",
+    justifyContent: "space-around",
     borderWidth: 1,
-    borderColor: 'gray',
-    padding: 10,
     borderRadius: 8,
-    // marginBottom: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
+    padding: 10,
+    gap: 8,
+    marginBottom: 20,
+    borderColor: "#ADB0CD",
+    margin: 20,
   },
-  textInput:{
-    // marginLeft: 10
+  menuIcon: {
+    marginTop: 7,
+    marginStart: 10,
+    marginEnd: 10,
+  },
+  inputTitle: {
+    flex: 1,
+    fontSize: 16,
+  },
+  loggedUserAvatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginLeft: 10,
+  },
+  titleRecieved: {
+    fontSize: 16,
+    fontWeight: "400",
+    marginHorizontal: 20,
+    marginBottom: 10,
+  },
+  containerEmail: {
+    flexDirection: "row",
+    padding: 10,
+    gap: 8,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  emailContent: {
+    padding: 10,
+    flex: 1,
+  },
+  emailSender: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  emailSubject: {
+    fontSize: 16,
+  },
+  emailBody: {
+    fontSize: 14,
+  },
+  userAvatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    marginStart: 5,
+  },
+  emailTime: {
+    fontSize: 14,
   },
 });
